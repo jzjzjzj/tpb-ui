7111a9380d8e0d2f5723d2e96bcde21c93c7cc9b
Jānis Z <jzjzjzj.fwd@gmail.com>
2014-10-22 00:48:48 +0300

    Set default columns on update if none set.

--- src/background.js
+++ src/background.js
@@ -13,4 +13,11 @@ chrome.runtime.onInstalled.addListener(function (details) {
   if(details.reason === 'install') {
     chrome.storage.local.set(columns);
   }
+  else if(details.reason === 'update') {
+    chrome.storage.local.get(null, function(items) {
+      if(Object.getOwnPropertyNames(items).length === 0) {
+        chrome.storage.local.set(columns);
+      }
+    });
+  }
 });
