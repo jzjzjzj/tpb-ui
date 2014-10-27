c2cb7f2d33e57a7c01a52a1e93343e3cea825e0c
Jānis Z <jzjzjzj.fwd@gmail.com>
2014-10-27 05:39:27 +0200

    Do not touch 'double' view yet.

--- src/js.js
+++ src/js.js
@@ -1,5 +1,11 @@
 'use strict';
 
+// do not touch "double" view
+if(document.cookie.indexOf('lw=d') !== -1) {
+  document.getElementById('searchResult').style.visibility = 'visible';
+  return;
+}
+
 require('angular/angular.min');
 require('./util');
 require('./tpb-ui');
