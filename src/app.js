2ea666a449c7feda66cb97e006ebf4e743548c59
Jānis Z <jzjzjzj.fwd@gmail.com>
2014-11-15 02:48:49 +0200

    Refactored entry script.

--- src/app.js
+++ src/app.js
@@ -1,16 +1,27 @@
 'use strict';
 
-// do not touch "double" view
-if(document.cookie.indexOf('lw=s') === -1) {
-  document.getElementById('searchResult').style.visibility = 'visible';
-  return;
-}
+var singleView = document.cookie.indexOf('lw=s') !== -1;
 
-require('angular/angular.min');
-require('./util');
-require('./tpb-ui');
+var libs = function () {
+  if(!singleView) {
+    return;
+  }
 
-angular.bootstrap(
-  angular.element('<search-result/>'),
-  ['tpb-ui', 'util']
-);
+  require('angular/angular.min');
+  require('./util');
+  require('./tpb-ui');
+};
+
+var boot = function() {
+  if(!singleView) {
+    return;
+  }
+
+  angular.bootstrap(
+    angular.element('<search-result/>'),
+    ['tpb-ui', 'util']
+  );
+};
+
+libs();
+boot();
