969d34c078ea4d248e8958f3428525b2dce9a0b9
Jānis Z <jzjzjzj.fwd@gmail.com>
2015-10-23 07:19:53 +0300

    Helper function for injecting scripts.

---
+++ src/inject.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function injectScript(path) {
+  var script = document.createElement('script');
+
+  script.src = chrome.extension.getURL(path);
+
+  (document.head||document.documentElement).appendChild(script);
+  script.parentNode.removeChild(script);
+}
